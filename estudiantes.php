7b478e161b0dd54426219584e386f0cd25ba8fe6
EdisonShañay <edisonsh9@gmail.com>
2022-06-20 22:20:39 -0500

    primer commit

---
+++ estudiantes.php
@@ -0,0 +1,16 @@
+<?php
+require_once('Conection.php');
+
+class Estudiantes extends Conection{
+
+	function listar_estudiantes(){
+		$resultado=$this->con->query('SELECT * FROM estudiantes');
+		return $resultado->fetch_all(MYSQLI_ASSOC);//
+	}
+
+
+}
+
+
+
+?>
---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+
+
+<style>
+  h1{
+    background: white ;/*color de fondo*/
+    color: white;/*color del texto*/
+    border: solid 1px black;
+    border border-radius: 5px  ;
+    font size:  ;
+    text-align:center; 
+    font-family: Arial ;
+    box-shadow: 5px 5px 10px black;
+   
+
+ }
+
+ .menu{
+
+  background: #9DA9A0 ; 
+  text-decoration:white;
+  color: black;
+  font-style:  ;
+  box-shadow: 5px 6px 10px black;
+
+
+ }
+
+
+ .menu:hover{
+
+  background: white;
+  color: black;
+  transition: 0.3s ;
+  border: solid;
+ }
+
+  .th{
+      background: green;
+  color: black;
+  transition: 0.2s ;
+  border: solid;
+
+
+  }
+
+
+</style>
+
+<h1 style="background:#655555">Menu Principal</h1>
+  <a class="menu" href=" lista_estudiantes.php">Estudiantes</a>
+  <a class="menu" href="lista_usuarios.php">Usuarios</a>
+  <img src="">
+  
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>REGISTRO</title>
+</head>
+<body style="background:#FFFFFF >
+   
+  
+
+
+</body>
+</html>
